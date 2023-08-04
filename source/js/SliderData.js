b4b2f0c8b5b2b41664d4befe714a4523394537bb
Elizaveta <isabethia7@gmail.com>
2023-08-04 14:33:35 +0300

    .

---
+++ source/js/SliderData.js
@@ -0,0 +1,17 @@
+/*
+
+export const SliderData = [
+  {
+    image:
+      './../img/header-coffee-desktop-cream@2x.png'
+  },
+  {
+    image:
+      './../img/header-coffee-desktop-espresso@2x.png'
+  },
+  {
+    image:
+      './../img/header-coffee-tablet-lavender@2x.png'
+  }
+];
+*/
