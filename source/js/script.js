42713032818f5a174f0f2b41e0bb5f4cdbc55242
Elizaveta <isabethia7@gmail.com>
2023-08-25 19:09:01 +0300

    toggle done

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,7 @@
+const navToggle = document.querySelector('.nav__toggle');
+const navDropdown = document.querySelector('.nav__dropdown');
+
+navToggle.addEventListener('click', () => {
+  navDropdown.classList.toggle('show');
+});
 
